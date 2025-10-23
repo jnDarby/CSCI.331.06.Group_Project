5730dace9c2cae21c4f5fee198a1592d79d75914
Emma Schmitt <eks6233@rit.edu>
2025-10-22 20:18:18 -0400

    Inital Commit, reading the CSV file and printing out the neighbors of Rochester

---
+++ dfs.py
@@ -0,0 +1,33 @@
+import math
+import csv
+
+filename = 'data.csv'
+
+allCities = []
+graph = {}
+
+with open(filename, newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    header = next(reader)  # read the header row with city names
+    allCities = header[0:]  # skip the first empty cell adn get the city names next 
+
+    with open(filename, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        header = next(reader)
+        allCities = header[0:]
+
+        for row in reader:
+            city_from = row[0]# its the first cell in the city name
+            distances = row[1:] # rest are the distances
+
+            # now creating a dictionary for this city's neighbors
+            graph[city_from] = {}
+
+            for city_to, distance_str in zip(allCities, distances):
+                distance = float(distance_str)
+                if distance > 0: # only add if theres an actual route between the two
+                    graph[city_from][city_to] = distance
+
+
+    print("Graph Loaded Successfully!")
+    print(f"Rochesters Neighbors: {graph['Rochester']}") # all neighbors of rochester
