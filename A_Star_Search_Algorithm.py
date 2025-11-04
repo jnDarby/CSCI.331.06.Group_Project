0797732423472fe4dc88ff2b6b62ac7d4b26099a
jnDarby <jnd6300@g.rit.edu>
2025-11-04 13:34:56 -0500

    Latest Update
    New improvements to code

--- A_Star_Search_Algorithm.py
+++ A_Star_Search_Algorithm.py
@@ -6,47 +6,42 @@ filename = 'data.csv'
 allCities = []
 routes = {}
 
-def loadValues():
-    with open(filename, newline='') as csvfile:
-        reader = csv.reader(csvfile)
-        header = next(reader)  # Read the header row with city names
-        allCities = header[0:]  # Store values
-        
-        for row in reader:
-            city_from = row[0]  # first cell in the row is the city name
-            distances = row[1:]
+with open(filename, newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    header = next(reader)  # Read the header row with city names
+    allCities = header[0:]  # Store values
+    for row in reader:
+        city_from = row[0]  # first cell in the row is the city name            
+        distances = row[1:]
             
-            for city_to, distance_str in zip(allCities, distances):
-                distance = float(distance_str)
-                # Use tuple keys for clarity and avoid shortening
-                routes[(city_from,city_to)] = distance
+    for city_to, distance_str in zip(allCities, distances):
+        distance = float(distance_str)
+        # Use tuple keys for clarity and avoid shortening
+        routes[(city_from,city_to)] = distance
 
-        for startCity in allCities:
-            for endCity in allCities:
-                print(startCity + " and " + endCity + " distance is " + str(routes[(startCity,endCity)]))
+        for route in routes:
+            print(str(route) + " is " + str(routes[route]))
 
 def findShortestAPath():
+    shortestDistance = math.inf
+    current = 0
+    shortestPath = ""
     for startCity in allCities:
-        for endCity in allCities:
-            if startCity == endCity:
-                break
-            else:
-                aSearch(startCity, endCity, allCities)
+            current = aSearch(startCity, allCities)
+            if(current[0] < shortestDistance):
+                shortestDistance = current[0]
+                shortestPath = current[1]
+    print("The shortest path is " + shortestPath + " with a direct line distance of " + str(shortestDistance) + " miles.")
 
-def aSearch(start: str, end: str, cities):
-    totalDistance = 0
-    path = ""
-    currentCity = start
+def aSearch(start: str, cities):
     
-    for next in allCities:
-        estPathValue(currentCity, next)
-        
-def estPathValue():
-    return None
+    totalDistance = 0
+    path = str(start) + " -> All Cities"
+     
+    return (totalDistance,path)
     
 
 def main():
-    loadValues()
     findShortestAPath()
 
 if __name__ == "__main__":
