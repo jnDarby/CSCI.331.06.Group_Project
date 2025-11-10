03541eeb71b756d85e2e76ca2073f69e1468ba9b
jnDarby <jnd6300@g.rit.edu>
2025-11-10 17:40:47 -0500

    A Star Search (Complete?)
    I think this is done, I gotta double check later and make sure that this is acutally an A* Search algo, but I think it is.

--- A_Star_Search_Algorithm.py
+++ A_Star_Search_Algorithm.py
@@ -14,13 +14,13 @@ with open(filename, newline='') as csvfile:
         city_from = row[0]  # first cell in the row is the city name            
         distances = row[1:]
             
-    for city_to, distance_str in zip(allCities, distances):
-        distance = float(distance_str)
-        # Use tuple keys for clarity and avoid shortening
-        routes[(city_from,city_to)] = distance
+        for city_to, distance_str in zip(allCities, distances):
+            distance = float(distance_str)
+            # Use tuple keys for clarity and avoid shortening
+            routes[(city_from,city_to)] = distance
 
-        for route in routes:
-            print(str(route) + " is " + str(routes[route]))
+    for route in routes:
+        print(str(route) + " is " + str(routes[route]))
 
 def findShortestAPath():
     shortestDistance = math.inf
@@ -28,18 +28,46 @@ def findShortestAPath():
     shortestPath = ""
     for startCity in allCities:
             current = aSearch(startCity, allCities)
+            
             if(current[0] < shortestDistance):
                 shortestDistance = current[0]
                 shortestPath = current[1]
     print("The shortest path is " + shortestPath + " with a direct line distance of " + str(shortestDistance) + " miles.")
 
-def aSearch(start: str, cities):
+def aSearch(next, cities):
     
+    distance = math.inf
     totalDistance = 0
-    path = str(start) + " -> All Cities"
-     
-    return (totalDistance,path)
+    path = str(next)
+    visited = []
+    visited.append(next)
     
+    while len(visited) < 30:
+        distance = math.inf
+        for end in cities:
+            if(end in visited or end == next):
+                None
+            else:
+                current = routes[(next, end)]
+                if((current + getEstimate(end)) < (distance + getEstimate(next))):
+                    distance = current
+                    next = end
+        print(distance)
+        visited.append(next)
+        totalDistance += distance
+        path += " -> " + str(next)
+
+    totalDistance = round(totalDistance, 3)     
+    return (totalDistance,path)
+
+def getEstimate(city):
+    shortest = math.inf
+    for next in allCities:
+        if(city != next):
+            distance = routes[(city, next)]
+            if( distance < shortest):
+                shortest = distance
+    return shortest
 
 def main():
     findShortestAPath()
